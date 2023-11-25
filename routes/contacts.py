ba474f23bced60cadee73cf416087a8dde25f490
Navarroseb <sebastnavarro@gmail.com>
2023-11-24 21:20:41 -0300

    Routes & Templates

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,10 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 contacts = Blueprint('contacts', __name__)
 
+
 @contacts.route('/')  # url /
 def home():  # función de la url /
-    return "Listado de contactos"
+    return render_template('index.html')
+
+
 @contacts.route('/new')
 def add_contact():
     return "Agregar contactos"
+
+@contacts.route('/about')  
+def about():  
+    return render_template('about.html')
+
+
+@contacts.route('/update')
+def update():
+    return "Updating a contact"
+
+
+@contacts.route('/delete')
+def delete():
+    return "Deleting contact"
